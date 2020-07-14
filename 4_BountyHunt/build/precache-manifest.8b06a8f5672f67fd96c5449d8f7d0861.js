f45ed5dac1d0b2b5802dad76ba68208395d7529f
Kassavin <kassavin001@gmail.com>
2020-07-14 14:08:26 +0100

    first commit

---
+++ 4_BountyHunt/build/precache-manifest.8b06a8f5672f67fd96c5449d8f7d0861.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd5f4cf35147774c1f2d",
+    "url": "/Ethereum_DApps/4_BountyHunt/build/static/js/runtime~main.cd5f4cf3.js"
+  },
+  {
+    "revision": "1eea87f6a244d8504ac5",
+    "url": "/Ethereum_DApps/4_BountyHunt/build/static/js/main.1eea87f6.chunk.js"
+  },
+  {
+    "revision": "38c984fed600bef067f1",
+    "url": "/Ethereum_DApps/4_BountyHunt/build/static/js/1.38c984fe.chunk.js"
+  },
+  {
+    "revision": "1eea87f6a244d8504ac5",
+    "url": "/Ethereum_DApps/4_BountyHunt/build/static/css/main.79c790f6.chunk.css"
+  },
+  {
+    "revision": "38c984fed600bef067f1",
+    "url": "/Ethereum_DApps/4_BountyHunt/build/static/css/1.fb95f01f.chunk.css"
+  },
+  {
+    "revision": "313c542e345b2b18399a342b66c59412",
+    "url": "/Ethereum_DApps/4_BountyHunt/build/index.html"
+  }
+];
---
+++ 5_String_Handling/build/precache-manifest.1e1c92383e2127980b1ed24fa733a67f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cf6caad6aa3370e481afaf0a878f728",
+    "url": "/Ethereum_DApps/5_String_Handling/build/index.html"
+  },
+  {
+    "revision": "c314f5034bff032c364a",
+    "url": "/Ethereum_DApps/5_String_Handling/build/static/css/main.dc0a14bb.chunk.css"
+  },
+  {
+    "revision": "e32ce822fd4aaf6616a1",
+    "url": "/Ethereum_DApps/5_String_Handling/build/static/js/2.e04050e8.chunk.js"
+  },
+  {
+    "revision": "c0b2ff8bc3820a7a15f78f42022ce72a",
+    "url": "/Ethereum_DApps/5_String_Handling/build/static/js/2.e04050e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c314f5034bff032c364a",
+    "url": "/Ethereum_DApps/5_String_Handling/build/static/js/main.dce9cfe7.chunk.js"
+  },
+  {
+    "revision": "efdeb7889c8a5897b56b",
+    "url": "/Ethereum_DApps/5_String_Handling/build/static/js/runtime-main.1932edd0.js"
+  }
+]);
